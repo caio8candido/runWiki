6685de994ffabda2fe0ed27f0fb49cf8ec5f8d02
Caio Candido <caio_euro@hotmail.com>
2018-12-18 14:46:00 -0200

    Updates

---
+++ precache-manifest.ca29cf89a60376dc24ec731a9c922a40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1e95420950986c7b3d7",
+    "url": "/runWiki/static/js/runtime~main.d1e95420.js"
+  },
+  {
+    "revision": "2c5f3381f786795202d9",
+    "url": "/runWiki/static/js/main.2c5f3381.chunk.js"
+  },
+  {
+    "revision": "846e098a801d8cb491f7",
+    "url": "/runWiki/static/js/1.846e098a.chunk.js"
+  },
+  {
+    "revision": "2c5f3381f786795202d9",
+    "url": "/runWiki/static/css/main.a7a0f30c.chunk.css"
+  },
+  {
+    "revision": "77315f4ccc53008b3702db11bbddf0fc",
+    "url": "/runWiki/index.html"
+  }
+];
